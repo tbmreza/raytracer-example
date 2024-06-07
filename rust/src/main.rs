5f8c511bbe60df50ebb2c10253368a76b3259d5c
Reza <rezahandzalah@gmail.com>
2024-06-07 22:38:27 +0700

    rust: dot

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,55 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul};
 
-#[derive(Default)]
+#[derive(Default, Debug, PartialEq, Clone)]
 struct Tuple(f64, f64, f64, f64);
+
+impl Tuple {
+    fn vector(x: f64, y: f64, z: f64) -> Self {
+    // ?? fn vector(x: Into<f64>, y: f64, z: f64) -> Self {
+        Tuple(x, y, z, 0.0)
+    }
+    fn dot(&self, rhs: &Tuple) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+}
 
 trait Point {
     fn is_point(&self) -> bool;
 }
+
 trait Vector {
     fn is_vector(&self) -> bool;
+    fn mag(&self) -> f64;
+    // fn reflect(&self, rhs: Box<dyn Vector>) -> Self;
+    // fn reflect(&self, rhs: impl Vector) -> Self;
+    fn reflect(&self, rhs: Tuple) -> Self;
 }
+
 impl Point for Tuple {
     fn is_point(&self) -> bool {
         self.3 == 1.0
     }
 }
+
 impl Vector for Tuple {
     fn is_vector(&self) -> bool {
         self.3 == 0.0
     }
+    fn mag(&self) -> f64 {
+        let sum = self.0 + self.1 + self.2 + self.3;
+        sum.sqrt()
+    }
+
+    // fn reflect(&self, normal: Tuple + Vector) -> Self {  // ??
+    fn reflect(&self, normal: Tuple) -> Self {
+        self.clone() - normal.clone() * 2.0 * self.dot(&normal)
+    }
+
 }
+
+// fn reflect(incoming: Vector, rhs: Vector) -> Vector {
+//     unimplemented!()
+// }
 
 impl Add for Tuple {
     type Output = Self;
@@ -27,58 +58,63 @@ impl Add for Tuple {
     }
 }
 
-fn main() {
-    let res = Tuple::default() + Tuple::default();
+impl Sub for Tuple {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Tuple(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2, self.3 - rhs.3)
+    }
 }
 
-// (module+ test (require rackunit)
-//   ; Scenario: A tuple with w=1.0 is a point
-//   (check-true (point? (tuple 4.3 -4.2 3.1 1.0)))
-//   (check-false (vec? (tuple 4.3 -4.2 3.1 1.0)))
-//
-//   ; Scenario: A tuple with w=0 is a vector
-//   (check-false (point? (tuple 4.3 -4.2 3.1 0.0)))
-//   (check-true (vec? (tuple 4.3 -4.2 3.1 0.0)))
-//
-//   ; Scenario: Adding two tuples
-//   (check-equal? (add (tuple 3 -2 5 1) (tuple -2 3 1 0))
-//                 (tuple 1 1 6 1))
-//
-//   ; Scenario: Subtracting two points
-//   (check-equal? (sub (point 3 2 1) (point 5 6 7))
-//                 (vec -2 -4 -6))
-//
-//   ; Scenario: Negating a tuple
-//   (check-equal? (neg (tuple 1 -2 3 -4))
-//                 (tuple -1 2 -3 4))
-//
-//   ; Scenario: Multiplying a tuple by a scalar
-//   (check-equal? (mul (tuple 1 -2 3 -4) 3.5)
-//                 (tuple 3.5 -7.0 10.5 -14.0))
-//
-//   ; Scenario: Computing the magnitude of vector
-//   (check-equal? (mag (vec 0 1 0))
-//                 1.0)
-//
-//   ; Scenario: The cross product of two vectors
-//   (define a (vec 1 2 3))
-//   (define b (vec 2 3 4))
-//   (check-equal? (cross a b)
-//                 (vec -1 2 -1))
-//   (check-equal? (cross b a)
-//                 (vec 1 -2 1))
-//
-//   ; Scenario: Colors are (red, green, blue) tuples
-//   (check-equal? (color-red (color -0.5 0.4 1.7))
-//                 -0.5)
-//   (check-equal? (color-green (color -0.5 0.4 1.7))
-//                 0.4)
-//
-//   ; Scenario: Multiplying a color by a scalar
-//   (check-equal? (mul (color 0.2 0.3 0.4) 2)
-//                 (color 0.4 0.6 0.8))
-//
-//   ; Scenario: Multiplying colors
-//   (check-equal? (product (color 1. 0.2 0.4) (color 0.9 1. 1.))  ; ?? 0.4 * 0.1 ieee bug
-//                 (color 0.9 0.2 0.4))
-//   )
+impl Mul<f64> for Tuple {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Tuple(self.0 * rhs, self.1 * rhs, self.2 * rhs, self.3 * rhs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn point_tuple() {
+        let w1 = Tuple(4.3, -4.2, 3.1, 1.0);
+        assert!(w1.is_point());
+        assert!(!w1.is_vector());
+    }
+
+    #[test]
+    fn point_vector() {
+        let w0 = Tuple(4.3, -4.2, 3.1, 0.0);
+        assert!(w0.is_vector());
+        assert!(!w0.is_point());
+    }
+
+    #[test]
+    fn ops_mul() {
+        let t = Tuple(1.0, -2.0, 3.0, -4.0) * 3.5;
+        let r = Tuple(3.5, -7.0, 10.5, -14.0);
+        assert_eq!(t, r);
+    }
+
+    #[test]
+    fn reflect_45() {
+        let v = Tuple::vector(1.0, -1.0, 0.0);
+        let n = Tuple::vector(0.0, 1.0, 0.0);
+        let r = Tuple::vector(1.0, 1.0, 0.0);
+        assert_eq!(v.reflect(n), r)
+    }
+
+    #[test]
+    fn reflect() {
+        let v = Tuple::vector(0.0, -1.0, 0.0);
+        // let n = Tuple::vector(); PICKUP  Reflecting a vector off a slanted surface
+        // let r = Tuple::vector(1.0, 1.0, 0.0);
+        // assert_eq!(v.reflect(n), r)
+    }
+
+}
+
+fn main() {
+    let _res = Tuple::default() + Tuple::default();
+}
